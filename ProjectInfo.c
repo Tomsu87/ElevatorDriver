0670bc1a3c6434acdc3cd1ba6d86741c6f81d25c
Tomsu87 <tomaszrluchowski@gmail.com>
2017-05-31 19:59:32 +0200

    Info Add
    Added input/output test config

---
+++ ProjectInfo.c
@@ -0,0 +1,31 @@
+/*
+
+Inputs :
+1 - safety status check.
+2 - Up button
+3 - Down button
+4 - Alert button
+5 - Upper position switch (NON)
+6 - Lower position switch (NON)
+7 - ??
+8 - ??
+
+
+Outputs :
+1 - Automatic locks
+2 - Motor up relay
+3 - Motor down relay
+4 - Motor allow motion relay
+5 - Up button light (upper alarm conn)
+6 - Down button light (lower alarm conn)
+7 - Alert light
+8 - Alert relay (buzzer -> gnd connection)
+
+
+All outputs 0-250V rated.
+All inputs 5-30V rated.
+
+
+
+
+*/
